b263d0a5dc861d256184dbf20fa486821d3357dd
eraliabd <eraliabdinazarov@gmail.com>
2024-11-29 21:02:27 +0500

    fix

--- noy29back6A.py
+++ noy29back6A.py
@@ -1,4 +1,14 @@
 
 print("Hello")
 
-# git init -
+# git init - Git repository yaratish
+# git add . - Fayllarni qo‘shish
+# git commit -m "Birinchi commit" - Commit qilish
+
+# GitHub-dagi repository URL manzilini oling
+# git remote add origin https://github.com/username/my-project.git
+
+# push qilishda username va password so'rasa, username o'rniga token(PAT) ishlatib keting
+
+# git branch -M main
+# git push -u origin main
